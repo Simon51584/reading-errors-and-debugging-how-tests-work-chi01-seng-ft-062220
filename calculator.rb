8bc7fb9cb98964da1bf6b17faab06a9bd86f4fe1
Seth Starr <starrseth@hotmail.com>
2020-06-05 23:47:34 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,5 +2,6 @@ first_number = 10
 second_number = 4
 sum = first_number + second_number
 difference = first_number - second_number
+product = first_number * second_number
 
 # Add your variables here
